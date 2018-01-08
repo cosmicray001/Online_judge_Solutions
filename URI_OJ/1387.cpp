ffd9b407a756da16c55cfa972ac858e09459ab6c
cosmicray001 <32748530+cosmicray001@users.noreply.github.com>
2018-01-08 08:43:31 +0600

    Create 1387.cpp

---
+++ URI_OJ/1387.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+  int a, b;
+  while(scanf("%d %d", &a, &b) != EOF && a && b){
+    printf("%d\n", a + b);
+  }
+  
+  return 0;
+}
