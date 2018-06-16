e0bccb7567c1bb811d039d14c7b7cea51ae5ea1b
cosmicray001 <32748530+cosmicray001@users.noreply.github.com>
2018-06-17 00:37:34 +0600

    Update 1076.cpp

--- Light_OJ/1076.cpp
+++ Light_OJ/1076.cpp
@@ -1,41 +1,40 @@
 #include <bits/stdc++.h>
 #define le 1003
 using namespace std;
-int n[le];
-
-int fnc1(int mid, int len){
-    int sum  = 0, c = 1;
+int n[le], len, cap;
+int ok(int a){
+    int sum = 0, c = 1;
     for(int i = 0; i < len; i++){
         sum += n[i];
-        if(sum > mid){
+        if(sum > a){
             sum = n[i];
             c++;
         }
     }
     return c;
 }
-int fnc(int hi, int lo, int len, int can){
+int fnc(int lo, int hi){
     int mid, ans;
     while(lo <= hi){
-        mid = (lo + hi) / 2;
-        if(fnc1(mid, len) > can) lo = mid + 1;
-        else{
+        mid = (lo + hi) >> 1;
+        int x = ok(mid);
+        if(x == cap){
             ans = mid;
             hi = mid - 1;
         }
+        else if(x > cap) lo = mid + 1;
+        else hi = mid - 1;
     }
     return ans;
 }
 int main(){
-    int t, co = 0, len, can;
+    int t, co = 0;
     for(scanf("%d", &t); t--; ){
-        scanf("%d %d", &len, &can);
-        int sum = 0, maxx = 0;
-        for(int i = 0; i < len; scanf("%d", &n[i]), sum += n[i], maxx = max(maxx, n[i]), i++);
-        int ans = 0;
-        if(can >= len) ans = maxx;
-        else ans = fnc(sum, maxx, len, can);
-        printf("Case %d: %d\n", ++co, ans, can);
+        scanf("%d %d", &len, &cap);
+        int sum = 0, hi = 0;
+        for(int i = 0; i < len; scanf("%d", &n[i]), sum += n[i], hi = max(hi, n[i]), i++);
+        if(cap >= len) printf("Case %d: %d\n", ++co, hi);
+        else printf("Case %d: %d\n", ++co, fnc(hi, sum));
     }
     return 0;
 }
