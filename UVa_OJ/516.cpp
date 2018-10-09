ecb8ffdc49a313a60f7fd83c71bc450d9340457f
cosmicray001 <32748530+cosmicray001@users.noreply.github.com>
2018-10-09 07:06:15 +0600

    Add files via upload

--- UVa_OJ/516.cpp
+++ UVa_OJ/516.cpp
@@ -1,8 +1,10 @@
 #include <bits/stdc++.h>
-#define le 32776
+#define le 32767
+#define ll long long int
 using namespace std;
 int n[le >> 6];
 vector<int> v;
+vector<int> p;
 #define ck(ve) (n[ve >> 6] & (1 << ((ve >> 1) & 31)))
 #define st(ve) (n[ve >> 6] |= (1 << ((ve >> 1) & 31)))
 void se(){
@@ -11,14 +13,47 @@ void se(){
     v.push_back(2);
     for(int i = 3; i < le; i += 2) if(!ck(i)) v.push_back(i);
 }
-int main(){
-    string s;
-    while(getline(cin, s)){
-        if(s[0] == '0' && s.size() == 1) break;
-        for(int i = 0; i < s.size(); ){
-            bool f = 0;
-            while()
+void fnc(ll a){
+    vector<int> pri;
+    bool f = false;
+    for(int i = 0; i < v.size() && v[i] * v[i] <= a; i++){
+        if(a % v[i] == 0){
+            int p = 0;
+            while(a % v[i] == 0){
+                a /= v[i];
+                p++;
+            }
+            pri.push_back(p);
+            pri.push_back(v[i]);
         }
     }
-
+    if(a != 1){
+        pri.push_back(1);
+        pri.push_back(a);
+    }
+    for(int i = pri.size() - 1; i > 1; printf("%d %d ", pri[i], pri[i - 1]), i -= 2);
+    printf("%d %d\n", pri[1], pri[0]);
+    return;
+}
+int main(){
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+    se();
+    string s;
+    while(getline(cin, s) && s[0] != '0'){
+        int x = 0;
+        for(int i = 0; i < s.size(); i++){
+            if(s[i] == ' '){
+                p.push_back(x);
+                x = 0;
+            }
+            else x = x * 10 + s[i] - '0';
+        }
+        p.push_back(x);
+        ll ans = 1;
+        for(int i = 0; i < p.size(); i += 2) ans *= pow(p[i], p[i + 1]);
+        fnc(ans - 1);
+        p.clear();
+    }
+    return 0;
 }
